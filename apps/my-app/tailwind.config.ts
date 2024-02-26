402c9cb9d5406061a67c38144bf24903b8471b0b
Yuji Takaesu <taka.esu.user@gmail.com>
2024-02-26 10:29:27 +0900

    Remove default apps of apps/web and apps/docs

--- apps/my-app/tailwind.config.ts
+++ apps/my-app/tailwind.config.ts
@@ -7,7 +7,7 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    '../../packages/ui/**/*.{js,ts,jsx,tsx,mdx}',
+    '../../packages/ui/**/*.{js,ts,jsx,tsx,mdx}', // uiの中のファイルも読み込む
   ],
   plugins: [],
 };
