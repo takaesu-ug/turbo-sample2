e7a5708695c16b9be8df6938436c4b1ee76a358d
Yuji Takaesu <taka.esu.user@gmail.com>
2024-02-27 22:37:00 +0900

    Add Badge

--- apps/my-app/src/app/page.tsx
+++ apps/my-app/src/app/page.tsx
@@ -1,30 +1,47 @@
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@repo/ui';
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger, Badge } from '@repo/ui';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-3xl font-bold">my-app-1</h1>
 
-      <Accordion type="single" collapsible className="w-full">
-        <AccordionItem value="item-1">
-          <AccordionTrigger>Is it accessible?</AccordionTrigger>
-          <AccordionContent className="text-purple-600">
-            Yes. It adheres to the WAI-ARIA design pattern.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-2">
-          <AccordionTrigger>Is it styled?</AccordionTrigger>
-          <AccordionContent>
-            Yes. It comes with default styles that matches the other components aesthetic.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-3">
-          <AccordionTrigger>Is it animated?</AccordionTrigger>
-          <AccordionContent>
-            Yes. Its animated by default, but you can disable it if you prefer.
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
+      <div className="w-full">
+        <h2>アコーディオン</h2>
+        <Accordion type="single" collapsible className="w-full">
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionContent className="text-purple-600">
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-2">
+            <AccordionTrigger>Is it styled?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It comes with default styles that matches the other components aesthetic.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-3">
+            <AccordionTrigger>Is it animated?</AccordionTrigger>
+            <AccordionContent>
+              Yes. Its animated by default, but you can disable it if you prefer.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
+
+      <div className="w-full">
+        <h2>バッジ</h2>
+        <Badge>デフォルト</Badge>
+        <Badge variant="secondary" className="ml-2">
+          セカンダリ
+        </Badge>
+        <Badge variant="destructive" className="ml-2">
+          デスとラクティブ
+        </Badge>
+        <Badge variant="outline" className="ml-2">
+          アウトライン
+        </Badge>
+      </div>
     </main>
   );
 }
--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,1 +1,2 @@
 export * from './src/components/accordion';
+export * from './src/components/badge';
