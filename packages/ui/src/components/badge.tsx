d93fa628d0b4a770476b92abbc0e57e532db83dd
Yuji Takaesu <taka.esu.user@gmail.com>
2024-03-17 16:48:39 +0900

    Fix

--- packages/ui/src/components/badge.tsx
+++ packages/ui/src/components/badge.tsx
@@ -1,7 +1,5 @@
-import { cva, type VariantProps } from 'class-variance-authority';
-import * as React from 'react';
-
 import { cn } from '@repo/ui/lib/utils';
+import { cva, type VariantProps } from 'class-variance-authority';
 
 const badgeVariants = cva(
   'focus:ring-ring inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2',
